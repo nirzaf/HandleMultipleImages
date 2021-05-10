5e0a4be8ded8e7ef7659e56bd734b75cf62e70bf
Quadrate Tech Solutions <info@quadrate.lk>
2021-05-10 13:06:19 +0530

    Add project files.

---
+++ Controllers/ImageHandlingController.cs
@@ -0,0 +1,67 @@
+﻿using HandleMultipleImages.Model;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HandleMultipleImages.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ImageHandlingController : ControllerBase
+    {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        [HttpPost("api/ImageUpload")]
+        public async Task<IActionResult> Index(List<IFormFile> files, List<Image> images)
+        {
+            ArrayList imageList = new();
+            var filePaths = new List<string>();
+            int i = 0;
+            foreach (var img in images)
+            {
+                    imageList.Add(images);   
+                    var filePath = Path.Combine(_hostingEnvironment.ContentRootPath, img.ImageName);
+                    
+                    //Image manipulation part any third party plugin or libraries can be used here 
+                    img.Effect1 = true ;
+                    img.Effect2 = false;
+                    img.Effect3 = true;
+                    img.Radius = 75;
+                    img.Size = 100;
+
+                    if (ImageValidators.IsExtensionValid(img.ImageName.ToString()))
+                    {
+                        filePaths.Add(filePath);
+                        var fileNameWithPath = string.Concat(filePath, "\\", img.ImageName);
+                        using var stream = new FileStream(fileNameWithPath, FileMode.Create);
+                        imageList.Add(img);
+                        await files[i].CopyToAsync(stream);
+                    }
+                i++;
+            }
+            // process uploaded files
+            return Ok(new { count = files.Count, filePaths , imageList });
+        }
+    }
+
+
+    public static class ImageValidators
+    {
+        public static bool IsExtensionValid(string fileName)
+        {
+            var extension = Path.GetExtension(fileName);
+
+            return string.Equals(extension, ".jpg", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(extension, ".png", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(extension, ".gif", StringComparison.OrdinalIgnoreCase) ||
+                   string.Equals(extension, ".jpeg", StringComparison.OrdinalIgnoreCase);
+        }
+    }
+}
---
+++ Model/Image.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace HandleMultipleImages.Model
+{
+
+    public partial class Image
+    {
+        [Key]
+        public int ImageID { get; set; }
+        public string ImageName { get; set; }
+
+        public bool Effect1 { get; set; }
+        public bool Effect2 { get; set; }
+        public bool Effect3 { get; set; }
+        public int Radius { get; set; }
+        public int Size { get; set; }
+    }
+}
